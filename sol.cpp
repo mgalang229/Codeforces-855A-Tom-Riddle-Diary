d3e8c0a0dbacdef835c900eb6918bed5287b0613
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-16 19:34:12 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,33 +1,39 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
 	int n;
 	cin >> n;
 	string s[n];
 	map<string, bool> mp;
-	for (int i = 0; i < n; i++) {
+	for(int i=0; i<n; ++i) {
 		cin >> s[i];
-		mp[s[i]] = false;
+		mp[s[i]]=0;
 	}
-	bool repeated[n + 1] = {false};
-	for (int i = 0; i < n; i++) {
-		if (!mp[s[i]]) {
-			mp[s[i]] = true;
-		} else {
-			repeated[i] = true;
-		}
+	bool vis[n+1];
+	memset(vis, 0, sizeof(vis));
+	for(int i=0; i<n; ++i) {
+		if(!mp[s[i]])
+			mp[s[i]]=1;
+		else
+			vis[i]=1;
 	}
-	for (int i = 0; i < n; i++) {
-		if (repeated[i]) {
+	for(int i=0; i<n; ++i) {
+		if(vis[i])
 			cout << "YES";
-		} else {
+		else
 			cout << "NO";
-		}
-		cout << '\n';
+		cout << "\n";
 	}
-	return 0;
 }
