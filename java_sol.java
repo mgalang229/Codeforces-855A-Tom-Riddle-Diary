ad798d9f24a3f389fc39f6d8edd9ea82bc61c3f3
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-16 19:37:47 +0800

    Update java_sol.java

--- java_sol.java
+++ java_sol.java
@@ -2,30 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static final Reader in = new Reader();
+	static final PrintWriter out = new PrintWriter(System.out);
+
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		String[] s = new String[n];
-		Map<String, Boolean> mp = new HashMap<String, Boolean>();
-		for (int i = 0; i < n; i++) {
-			s[i] = in.next();
-			mp.put(s[i], false);
-		}
-		boolean[] repeated = new boolean[n + 1];
-		Arrays.fill(repeated, false);
-		for (int i = 0; i < n; i++) {
-			if (mp.get(s[i]) == false) {
-				mp.put(s[i], true);
-			} else {
-				repeated[i] = true;
+//		int t=in.nextInt();
+		int t=1;
+		for(int i=1; i<=t; ++i)
+			new Solver();
+		out.close();
+	}
+	
+	static class Solver {
+		Solver() {
+			int n=in.nextInt();
+			String[] s = new String[n];
+			Map<String, Boolean> mp = new HashMap<String, Boolean>();
+			for(int i=0; i<n; ++i) {
+				s[i]=in.next();
+				mp.put(s[i], false);
 			}
-		}
-		for (int i = 0; i < n; i++) {
-			if (repeated[i]) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
+			boolean[] vis = new boolean[n+1];
+			Arrays.fill(vis, false);
+			for(int i=0; i<n; ++i) {
+				if(mp.get(s[i])==false)
+					mp.put(s[i], true);
+				else
+					vis[i]=true;
+			}
+			for(int i=0; i<n; ++i) {
+				if(vis[i])
+					out.println("YES");
+				else
+					out.println("NO");
 			}
 		}
 	}
+	
+	static class Reader {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String next() {
+			while(st==null||!st.hasMoreTokens()) {
+				try {
+					st=new StringTokenizer(in.readLine());
+				} catch(Exception e) {}
+			}	
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
+	
 }
